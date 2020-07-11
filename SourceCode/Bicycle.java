bc6e7bf02ce899c3722026fab06006b0a93f1951
alis-jain <ajainapurviitbhu@gmail.com>
2020-07-12 04:32:36 +0530

    Removed nested class Wheel. Made a new seperate class Wheel with all functionality of previous nested class. Added functionality to calculate circumference of wheel. Making seperate class was necessary to make every class single responsible class.

---
+++ SourceCode/Bicycle.java
@@ -0,0 +1,50 @@
+import java.lang.Math.*;
+
+class Gear{	
+	int chainring;
+	int cog;
+	Wheel myWheel;
+
+	Gear(int chainring, int cog, Wheel myWheel){
+		this.chainring = chainring;
+		this.cog = cog;
+		this.myWheel = myWheel;
+	}
+
+	double gearInches(){
+		return gearRatio()*myWheel.wheelDiameter();
+	}
+
+	double gearRatio(){
+		return ((double)chainring)/cog;
+	}
+}
+
+class Wheel{
+	double rim;
+	double tire;
+
+	Wheel(double rim, double tire){
+		this.rim = rim;
+		this.tire = tire;
+	}
+
+	double wheelDiameter(){
+		return rim + 2*tire;
+	}
+
+	double wheelCircumference(){
+		return wheelDiameter()*Math.PI;
+	}
+}
+
+class Bicycle{
+	public static void main(String[] args){
+		Wheel myWheel = new Wheel(26, 1.5);
+		System.out.println("Circumference = " + myWheel.wheelCircumference());
+		Gear newGear = new Gear(52, 11, myWheel);
+		System.out.println("Ratio = " + newGear.gearRatio());
+		System.out.println("Inches = " + newGear.gearInches());
+	}
+}
+
