97a5b1ef0346592dce9100d0d7f989f644b460d7
alis-jain <ajainapurviitbhu@gmail.com>
2020-07-11 21:34:21 +0530

    Added new data members to class Gear, rim and tire. Added new member function gearInches which calculates affect of wheel size on gear quality. Changed name of member function ratio to gearRatio.

--- SourceCode/Gear.java
+++ SourceCode/Gear.java
@@ -1,13 +1,21 @@
 class Gear{
 	int chainring;
 	int cog;
+	double rim;
+	double tire;
 
-	Gear(int chainring, int cog){
+	Gear(int chainring, int cog, double rim, double tire){
 		this.chainring = chainring;
 		this.cog = cog;
+		this.rim = rim;
+		this.tire = tire;
 	}
 
-	double ratio(){
+	double gearInches(){
+		return gearRatio()*(rim + 2*tire);
+	}
+
+	double gearRatio(){
 		return (chainring*1.0)/cog;
 	}
 }
