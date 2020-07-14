6e1caab86402880c52d2b96ad968bb14999f7541
alis-jain <ajainapurviitbhu@gmail.com>
2020-07-15 02:57:03 +0530

    Added some new attributes in class Bicyle. If-Else statement defines some attributes based on value of some other attribute. This makes the way for making seperate class for these dependent features and using Inheritance.

--- SourceCode/BicycleType.java
+++ SourceCode/BicycleType.java
@@ -1,24 +1,36 @@
 class Bicycle{
-	String size;
+	String style, size;
 	String tape_color;
 	String chain, tire_size;
+	String front_shock, rear_shock;
 
-	Bicycle(String size, String tape_color){
+	Bicycle(String style, String size, String tape_color, String front_shock, String rear_shock){
+		this.style = style;
 		this.size = size;
 		this.tape_color = tape_color;
+		this.front_shock = front_shock;
+		this.rear_shock = rear_shock;
 	}
 
 	void spares(){
-		this.chain = "10-speed";
-		this.tire_size = "23";
+		if (style == "road"){
+			this.chain = "10-speed";
+			this.tire_size = "23";
+		}
+		else{
+			this.chain = "10-speed";
+			this.tire_size = "2.1";
+		}
+		
 	}
 }
 
 class BicycleType{
 	public static void main(String[] args){
-		Bicycle bike = new Bicycle("M", "Red");
+		Bicycle bike = new Bicycle("mountain", "S", "Red", "Manitou", "Fox");
 		bike.spares();
 		System.out.println("Size is " + bike.size);
 		System.out.println("Chain is " + bike.chain);
+		System.out.println("Chain is " + bike.tire_size);
 	}
 }
